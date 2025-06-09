f95e5f8a7cc25221b6045ef111eca76702c84c28
irshad <almairsh0205@gmail.com>
2025-06-09 16:45:09 +0530

    update fornt with deloyed link

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // replace with your frontend URL
+    origin: process.env.CLIENT_URL || "http://localhost:3000", // dynamic origin
     credentials: true,
-    optionsSuccessStatus: 200,
   })
 );
 
