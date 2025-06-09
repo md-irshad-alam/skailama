a6aff3bf9bd30f9fb9339b7458f4bc795a1ad8f5
irshad <almairsh0205@gmail.com>
2025-06-09 18:58:00 +0530

    update the env file

--- skailama/src/componants/auttPage.jsx
+++ skailama/src/componants/auttPage.jsx
@@ -54,6 +54,8 @@ export default function AuthPage() {
     }
   };
 
+  const [loading, setLoading] = useState(false);
+
   return (
     <>
       <ToastContainer />
@@ -99,7 +101,14 @@ export default function AuthPage() {
                 Welcome to <span className="text-purple-700">Ques.AI</span>
               </h2>
             </div>
-            <form className="space-y-4" onSubmit={handleSubmit}>
+            <form
+              className="space-y-4"
+              onSubmit={async (event) => {
+                setLoading(true);
+                await handleSubmit(event);
+                setLoading(false);
+              }}
+            >
               {isLogin == false && (
                 <div>
                   <label className="block text-sm font-medium text-gray-700 mb-1">
@@ -148,9 +157,36 @@ export default function AuthPage() {
               </div>
               <button
                 type="submit"
-                className="w-full bg-purple-700 text-white py-2 rounded hover:bg-purple-800 transition"
+                className="w-full bg-purple-700 text-white py-2 rounded hover:bg-purple-800 transition flex items-center justify-center"
+                disabled={loading}
               >
-                Login
+                {loading ? (
+                  <>
+                    <svg
+                      className="animate-spin h-5 w-5 mr-2 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                      ></path>
+                    </svg>
+                    Loading...
+                  </>
+                ) : (
+                  "Login"
+                )}
               </button>
               <div className="flex items-center gap-2">
                 <div className="border-b w-full" />
